039b18976e9dafb182dc88e9fc1ea0913cef764a
Jethro Vitug <jethrov2g@yahoo.com>
2020-12-10 17:02:11 +0800

    Laravel /w VueJS and InertiaJS

---
+++ database/seeders/SampleUser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class SampleUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'sample',
+        	'email' => 'demo@demo.com.au',
+        	'password' => Hash::make('hello2020'),
+        ]);
+    }
+}
